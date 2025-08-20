76f8fe4a11c453f01e45f81c5be033760cb37d30
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-08-21 01:00:37 +0400

    started final exam project

---
+++ src/components/Route.jsx
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import Home from "./Main/Home";
+import Footer from "./Footer/Footer";
+import { Cart } from "./Cart/Cart";
+import { Button } from "./Button/Button";
+
+
+export { Header, Home, Footer, Cart, Button };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+// const Home = React.lazy(() => import("./components/Route.jsx"));
+
+import { Home } from "./components/Route.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
