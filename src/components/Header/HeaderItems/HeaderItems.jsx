729a56f1fb918bd0999f3344321e80c3e1b63c23
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-08-30 01:28:48 +0400

    started login registration

--- src/components/Header/HeaderItems/HeaderItems.jsx
+++ src/components/Header/HeaderItems/HeaderItems.jsx
@@ -1,9 +1,17 @@
 import { IoIosHeartEmpty } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
 import { FiUser } from "react-icons/fi";
+import { UserCart } from "./UserCart";
+import { useState } from "react";
 import "./HeaderItems.scss";
 
 const HeaderItems = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleUser = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="header_items">
       <div className="favorites_cont">
@@ -14,9 +22,10 @@ const HeaderItems = () => {
         <IoCartOutline className="header_icon cart" />
         <p className="cart_icon icons">Cart</p>
       </div>
-      <div className="user_cont">
+      <div className="user_cont" onClick={toggleUser}>
         <FiUser className="header_icon user" />
         <p className="user_icon icons">Account</p>
+        {isOpen && <UserCart />}
       </div>
     </div>
   );
---
+++ src/components/Header/HeaderItems/UserCart.jsx
@@ -0,0 +1,12 @@
+import { FiLogIn } from "react-icons/fi";
+
+export const UserCart = () => {
+  return (
+    <div className="user_cart_container">
+      <p className="login_text">
+        <FiLogIn className="login_icon"/>
+        Log In
+      </p>
+    </div>
+  );
+};
