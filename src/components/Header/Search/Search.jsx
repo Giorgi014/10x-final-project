e4e87fbac8844452080df51b740860a5128c44c2
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-09-03 14:26:20 +0400

    search is done

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,14 +1,37 @@
 import { CiSearch } from "react-icons/ci";
 import { SearchedItem } from "./SearchedItem/SearchedItem";
+import { useData } from "../../Context/DataContext";
+import { useState } from "react";
 import "./Search.scss";
 
 export const Search = () => {
+  const { data } = useData();
+  const [searchItem, setSearchItem] = useState("");
+
+  const allImages = data?.allImages || [];
+
+  const filteredItems = allImages.filter((i) =>
+    (i.description || i.name || i.item_name)
+      ?.toLowerCase()
+      .includes(searchItem.toLowerCase())
+  );
+
   return (
     <article className="search">
-      <input type="text" className="search_input" placeholder="Search" />
+      <input
+        type="text"
+        className="search_input"
+        placeholder="Search"
+        value={searchItem}
+        onChange={(e) => setSearchItem(e.target.value)}
+      />
       <CiSearch className="search_icon" />
       <div className="searched_item">
-        <SearchedItem />
+        {searchItem && filteredItems.length > 0
+          ? filteredItems.map((item) => (
+              <SearchedItem key={item.id} item={item} />
+            ))
+          : searchItem && <p className="not_found">Items not found</p>}
       </div>
     </article>
   );
--- src/components/Header/Search/SearchedItem/SearchedItem.jsx
+++ src/components/Header/Search/SearchedItem/SearchedItem.jsx
@@ -1,10 +1,14 @@
 import "./SearchedItem.scss";
 
-export const SearchedItem = () => {
+export const SearchedItem = ({ item }) => {
   return (
     <div className="searched_item_contaner">
-      <div className="searched_img"></div>
-      <p className="searched_item_info">test text</p>
+      <img
+        src={item.url}
+        alt={item.item_name || item.description}
+        className="searched_img"
+      />
+      <p className="searched_item_info">{item.item_name || item.description}</p>
     </div>
   );
 };
