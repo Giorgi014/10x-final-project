3f7a58a171445e52fb6c7b14ed1fe71b9d297598
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-09-04 02:29:04 +0400

    about page is done

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,23 +1,47 @@
-import { useEffect } from "react";
-import { useLoader } from "../../Context/LoaderContext";
 import "./About.scss";
 
 const About = () => {
-  const { setIsLoading } = useLoader();
-
-  useEffect(() => {
-    setIsLoading(true)
-
-    return () =>{
-      setIsLoading(false)
-    }
-  }, [setIsLoading])
-
   return (
     <article className="about_container">
-      <h2>Maintenance work in progress</h2>
+      <h2 className="about_head">About Us</h2>
+      <p className="about_company">
+        Our company, the cyber chain of digital and home appliance stores, has
+        been the go-to place for tech upgrades since 1997.
+      </p>
+      <p className="about_company">
+        Our company, the cyber chain of digital and home appliance stores, has
+        been the go-to place for tech upgrades since 1997. During this time, we
+        have kept up with changes and followed the world's pace, providing our
+        loyal customers with modern technology for creativity, discovery,
+        development, self-expression, knowledge, progress, and positive change.
+      </p>
+      <p className="about_company">
+        Today, we have 16 stores in 7 cities across Georgia. In addition, we
+        have a digital sales space—the online store cyber.ge, for retail,
+        corporate, and wholesale purchases.
+      </p>
+      <p className="about_company">
+        We have official partnerships with the world's leading brands, including
+        Apple, Samsung, Lenovo, Asus, Sony, LG, DeLonghi, TCL, Honor, AEG,
+        Electrolux, Philips, Panasonic, Toshiba, Canon, and over 100 other
+        international companies. This is why you can buy modern, innovative, and
+        high-quality appliances from the most famous and reliable brands in the
+        world. Whether you need to upgrade your smartphone, computer, TV, home
+        appliances, or other devices and accessories, you will find an endless
+        selection of technology with us. At the same time, you'll benefit from
+        qualified service and service centers authorized by international
+        brands.
+      </p>
+      <p className="about_company">
+        We believe that innovative technologies are a tool for people to reveal
+        their potential and express themselves. That's why we want to help
+        people get better technology for a better life and experience the
+        endless possibilities of both themselves and technology.
+      </p>
+      <p className="about_company">Join us.</p>
+      <p className="about_company">It's time for an upgrade.</p>
     </article>
-  )
-}
+  );
+};
 
-export default About
+export default About;
